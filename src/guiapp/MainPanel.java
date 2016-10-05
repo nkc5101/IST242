9c90bc0119bcb0a6c00facd75a68396aec83626d
John Ide <ide.johnc@gmail.com>
2016-10-05 15:30:02 -0400

    spacing

--- src/guiapp/MainPanel.java
+++ src/guiapp/MainPanel.java
@@ -1,15 +1,17 @@
 package guiapp;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
+        
 public class MainPanel extends JPanel {
-MainPanel(){
-    JButton blueButton = new JButton("Blue");
-    add(blueButton);
-    
-    JButton redButton = new JButton("Red");
-    add(redButton);
-    
-    JButton johnIdeButton = new JButton("John Ide");
-    add(johnIdeButton);
-}
+    MainPanel() {
+        JButton blueButton = new JButton("Blue");
+        add(blueButton);
+
+        JButton redButton = new JButton("Red");
+        add(redButton);
+
+        JButton johnIdeButton = new JButton("John Ide");
+        add(johnIdeButton);
+    }
 }
