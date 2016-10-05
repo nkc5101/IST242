995308c435e7795b6102bc1b5d2298d8da7939a3
John Ide <ide.johnc@gmail.com>
2016-10-05 15:30:41 -0400

    spacing

--- src/guiapp/MainFrame.java
+++ src/guiapp/MainFrame.java
@@ -3,13 +3,13 @@ package guiapp;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class MainFrame extends JFrame{
-    MainFrame(){    
-super("Paint Program");
-    MainPanel mPanel = new MainPanel();
-    getContentPane().add(mPanel);
-    setSize(500, 400);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setVisible(true);
-}
+public class MainFrame extends JFrame {
+    MainFrame() {    
+        super("Paint Program");
+        MainPanel mPanel = new MainPanel();
+        getContentPane().add(mPanel);
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
